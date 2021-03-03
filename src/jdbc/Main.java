a29331cc05839160de5234ffbf4f55aba45d7b68
albi1 <albi1@10.60.91.81>
2021-03-03 20:21:56 +0100

    unfinished menu and started ccreating the sql interface

---
+++ src/jdbc/Main.java
@@ -0,0 +1,36 @@
+package jdbc;
+
+import java.util.*;
+
+public class Main {
+	static String username;
+	static String password;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Quiron's ER");
+		System.out.println("Please enter your username and password:");
+		System.out.println("Username:");
+		username = sc.next();
+		System.out.println("Password:");
+		password = sc.next();
+		if(username.equalsIgnoreCase("ms")&&password.equalsIgnoreCase("ms")) {
+			System.out.println("Choose an option[1-]:");
+			System.out.println("1.");
+			
+		}if(username.equalsIgnoreCase("as")&&password.equalsIgnoreCase("as")) {
+			System.out.println("Choose an option[1-]:");
+			System.out.println("1.");
+			
+		}if(username.equalsIgnoreCase("p")&&password.equalsIgnoreCase("p")) {
+			System.out.println("Choose an option[1-]:");
+			System.out.println(" 1.Create treatment \n 2.Edit treatment\n 3.");
+			
+		}else {
+			
+		}
+		
+
+	}
+
+}
---
+++ src/jdbc/SQLInterface.java
@@ -0,0 +1,10 @@
+package jdbc;
+
+import java.sql.*;
+
+public interface SQLInterface {
+	
+	public void connect(Connection c);
+	public void create();
+
+}
