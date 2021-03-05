8b14942e88b8275b2badcf6efd4217878fc1262a
albi1 <albi1@10.60.89.110>
2021-03-05 19:14:17 +0100

    connect method of class SQL need to be done

--- src/jdbc/SQL.java
+++ src/jdbc/SQL.java
@@ -1,13 +1,72 @@
 package jdbc;
 import java.sql.*;
 
-public class SQL {
-	
+public class SQL implements SQLInterface{
+
+	@Override
 	public void connect() {
-	
-}
-	public void create() {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void create(Connection c) throws SQLException {
+		// TODO table workers, 
+		Statement stmt1 = c.createStatement();
+		String sql1 = "CREATE TABLE patients "
+				   + "(id       INTEGER  PRIMARY KEY,"
+				   + " name     TEXT     NOT NULL, "
+				   + " surname     TEXT     NOT NULL, "
+				   + " gender     TEXT     NOT NULL, "
+				   + " blood type     TEXT     NOT NULL, "
+				   + " allergies     TEXT     NOT NULL, "
+				   + " birthdate     DATE     NOT NULL, "
+				   + " address  TEXT	 NOT NULL,"
+				   + "a)";
+		stmt1.executeUpdate(sql1);
+		stmt1.close();
+		Statement stmt2 = c.createStatement();
+		String sql2 = "CREATE TABLE employees "
+				   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+				   + " name     TEXT     NOT NULL, "
+				   + " dob      DATE	 NOT NULL, "
+				   + " address  TEXT, "
+				   + " salary   REAL,"
+				   + " photo	BLOB,"
+				   + " dep_id	INTEGER REFERENCES departments(id) ON UPDATE CASCADE ON DELETE SET NULL)";
+		stmt2.executeUpdate(sql2);
+		stmt2.close();
+		Statement stmt3 = c.createStatement();
+		String sql3 = "CREATE TABLE reports "
+				   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+				   + " name     TEXT     NOT NULL, "
+				   + " content  TEXT  	NOT NULL, "
+				   + " date		DATE)";
+		stmt3.executeUpdate(sql3);
+		stmt3.close();
+		Statement stmt4 = c.createStatement();
+		String sql4 = "CREATE TABLE authors "
+				   + "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+				   + " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+				   + " PRIMARY KEY (report_id,employee_id))";
+		stmt4.executeUpdate(sql4);
+		stmt4.close();
+		System.out.println("Tables created.");
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 	
 }
