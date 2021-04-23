26c110178d153a2b78e96a084b4ccad28c98b231
albi1 <albi1@10.60.90.57>
2021-04-23 20:28:51 +0200

    have modified de sql and finished the patients pojo

--- src/db/jdbc/SQLInterface.java
+++ src/db/jdbc/SQLInterface.java
@@ -10,7 +10,6 @@ public interface SQLInterface {
 	public Connection connect() throws SQLException, ClassNotFoundException;
 	public void create(Connection c) throws SQLException;
 	public void delete();
-	public void drop();
 	void drop(Connection c) throws SQLException;
 
 	/**
