47657a6b1f2faab3c573978d6ae4f89c0c58e61d
albi1 <albi1@10.60.90.20>
2021-05-11 19:43:20 +0200

    fixed some errors

--- src/db/jdbc/SQL.java
+++ src/db/jdbc/SQL.java
@@ -1,6 +1,4 @@
 package db.jdbc;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.rmi.NotBoundException;
 import java.sql.*;
 import java.util.ArrayList;
@@ -24,7 +22,7 @@ public class SQL implements SQLInterface{
 	
 	@Override
 	public void create(Connection c) throws SQLException {
-		Statement stmt1 = c.createStatement();
+			Statement stmt1 = c.createStatement();
 		String sql1 = "CREATE TABLE patients "
 				   + "(medical_card_number       INTEGER  PRIMARY KEY,"
 				   + " name     TEXT     NOT NULL, "
--- src/db/menu/Main.java
+++ src/db/menu/Main.java
@@ -1,12 +1,10 @@
 package db.menu;
  
-import java.io.InputStreamReader;
+
 import java.rmi.NotBoundException;
 import java.security.*;
 import java.sql.*;
 import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.chrono.ThaiBuddhistDate;
 import java.util.*;
 
 import db.pojos.*;
@@ -27,7 +25,13 @@ public class Main {
 		try{
 			c= jdbc.connect();
 			userman.connect();
-			jdbc.create(c);
+			try{
+				jdbc.create(c);
+			}catch(SQLException ex) {
+				if(!ex.getMessage().contains("already exists")) {
+					ex.printStackTrace();
+				}
+			}
 			do {
 				System.out.println("Welcome to the Quiron's ER");
 				System.out.println("1. Register");
