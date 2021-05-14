8c6145ac0c30bcdc0bb64949ec4049a758c52416
hugoperezgi <Hugo@Hugoo.home>
2021-05-14 13:12:27 +0200

    javaFx stuff

---
+++ src/db/ui/Main.java
@@ -0,0 +1,16 @@
+package db.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+		launch(args);
+    }
+
+	@Override
+	public void start(Stage stage0) throws Exception {
+		// TODO Auto-generated method stub
+		stage0.show();
+	}
+}
---
+++ src/db/ui/SceneController.java
@@ -0,0 +1,5 @@
+package db.ui;
+
+public class SceneController {
+    
+}
