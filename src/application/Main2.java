586fbb0b14c8441f8c1a493c9d58f4134062c144
hugoperezgi <Hugo@Hugoo>
2021-05-18 20:18:57 +0200

    javafx Stuff

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -1,19 +1,39 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
+import java.rmi.NotBoundException;
+
+import java.security.*;
+import java.sql.*;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.*;
+
+import db.pojos.*;
+import pojos.users.*;
+import db.jdbc.*;
+import db.jpa.JPAUserManager;
 
 public class Main2 extends Application {
+
+	// static Connection c ;
+	// static SQL jdbc = new SQL();
+	// private static JPAUserManager userman = new JPAUserManager();
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("logInMenu.fxml"));
+			Scene scene = new Scene(root,824,601);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/application/UIcontroller.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class UIcontroller {
+    
+
+    /**
+     * Activates when user clicks on "OK" in the main log in window.
+     * 
+     * @param aEvent
+     */
+    @FXML
+    public void logInMenuOK(ActionEvent aEvent) {
+        
+    }
+
+}
