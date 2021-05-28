0be0f1921278b16e17fbd3e0f930302f4bcca6d4
Gisela Aragon <giselaaragondomenech@gmail.com>
2021-05-29 00:08:59 +0200

    Javafx

--- src/application/controllers/PatientMenuController.java
+++ src/application/controllers/PatientMenuController.java
@@ -12,19 +12,20 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.rmi.NotBoundException;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import db.jdbc.*;
 import db.jpa.*;
 import db.pojos.*;
 
-// import db.jdbc.*;
 
-public class PatientMenuController {
+
+public class PatientMenuController implements Initializable{
 
 	private static SQL jdbc;
 	private static JPAUserManager userman;
@@ -38,17 +39,17 @@ public class PatientMenuController {
     @FXML
     TableView<Treatment> treatmentsTable;
     @FXML
-    TableColumn<Treatment, String> treatmentId;
+    TableColumn<Treatment, String> treatmentId = new TableColumn<>("treatmentId");
     @FXML
-    TableColumn<Treatment, String> dateTreatment;
+    TableColumn<Treatment, String> startDate = new TableColumn<>("startDate");
     @FXML
-    TableColumn<Treatment, String> durationTreatment;
+    TableColumn<Treatment, String> duration = new TableColumn<>("duration");
     @FXML
-    TableColumn<Treatment, String> medicationTreatment;
+    TableColumn<Treatment, String> medication = new TableColumn<>("medication");
     @FXML
-    TableColumn<Treatment, String> diagnosisTreatment;
+    TableColumn<Treatment, String> diagnosis = new TableColumn<>("diagnosis");
     @FXML
-    TableColumn<Treatment, String> adviceTreatment;
+    TableColumn<Treatment, String> recommendation = new TableColumn<>("recommendation");
 
 	@FXML
 	Pane consultTreatmentsView;
@@ -67,6 +68,13 @@ public class PatientMenuController {
 	public static PatientMenuController passPatientMenuController() {
 		return thisPatientMenuController;
 	}
+	
+	// Hide all
+	private void hideAll() {
+		consultTreatmentsView.setVisible(false);
+		consultTreatmentsView.setDisable(true);
+	}
+
 
 	public void displayPatientWelcomeText(Patient p, SQL sqlman, JPAUserManager userm) {
 		try {
@@ -78,48 +86,64 @@ public class PatientMenuController {
 			e.printStackTrace();
 		}
     }
-    //TODO gisel no se crea la tabla pqq nunca llamas a las funciones en el display que es a donde accede javafx y hay que cambiar los ajustes del label pqq no se ve el nombre
-   /* private void selectTreatment() {
-    	treatmentId.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getTreatmentId())));
-    	dateTreatment.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getStartDate().toString()));
-    	durationTreatment.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getDuration())));
-    	medicationTreatment.setCellValueFactory(new PropertyValueFactory<>("medicationTreatment"));
-    	adviceTreatment.setCellValueFactory(new PropertyValueFactory<>("adviceTreatment"));
-    	diagnosisTreatment.setCellValueFactory(new PropertyValueFactory<>("diagnosisTreatment"));
-   }*/
-    private void setTreatmentTables() throws Exception{
-    	
-        List<Treatment> treatmentList = new ArrayList<>();
-        treatmentList.addAll(jdbc.selectAllTreatments(patient.getMedicalCardId()));
-        if (treatmentList.isEmpty()) {
-        	ErrorPopup.errorPopup(7);
-        	displayPatientWelcomeText(patient, jdbc, userman);
-        } else {
-        treatmentsTable.getItems().clear();
-        treatmentsTable.getColumns().clear();
-        treatmentsTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        treatmentId.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getTreatmentId())));
-        DateFormat dateformat=new SimpleDateFormat("yyyy-MM-dd");
-    	dateTreatment.setCellValueFactory(data -> new SimpleStringProperty(dateformat.format(data.getValue().getStartDate())));
-    	durationTreatment.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getDuration())));
-    	medicationTreatment.setCellValueFactory(new PropertyValueFactory<>("medicationTreatment"));
-    	adviceTreatment.setCellValueFactory(new PropertyValueFactory<>("adviceTreatment"));
-    	diagnosisTreatment.setCellValueFactory(new PropertyValueFactory<>("diagnosisTreatment"));
-        treatmentsTable.getColumns().addAll(treatmentId, dateTreatment, durationTreatment, medicationTreatment, diagnosisTreatment, adviceTreatment);
-        treatmentsTable.getItems().addAll(treatmentList);
-        }
-    }
-
-    public void displayAllTreatmentsView(ActionEvent aEvent) {
-    	consultTreatmentsView.setVisible(false);
-        consultTreatmentsView.setDisable(false);
-        try {
-			setTreatmentTables();
+	
+	private static Patient currentPatient = new Patient();
+	
+	private void setTreatmentTables() throws IOException, NumberFormatException, NotBoundException, SQLException {
+		if (currentPatient == null) {
+			ErrorPopup.errorPopup(4);
+			return;
+		}
+		List<Treatment> treatments = new ArrayList<>();
+		try {
+			treatments.addAll(jdbc.selectAllTreatments(currentPatient.getMedicalCardId()));
 		} catch (Exception e) {
 			e.printStackTrace();
+			ErrorPopup.errorPopup(0);
+			return;
 		}
-        consultTreatmentsView.setVisible(true);
-    }
+		if (treatments.isEmpty()) {
+			ErrorPopup.errorPopup(7);
+			displayPatientWelcomeText(patient, jdbc, userman);
+		} else {
+			treatmentsTable.getItems().clear();
+			treatmentsTable.getColumns().clear();
+			startDate
+					.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getStartDate().toString()));
+			duration.setCellValueFactory(
+					data -> new SimpleStringProperty(Integer.toString(data.getValue().getDuration())));
+			medication.setCellValueFactory(new PropertyValueFactory<>("medication"));
+			recommendation.setCellValueFactory(new PropertyValueFactory<>("recommendation"));
+			diagnosis.setCellValueFactory(new PropertyValueFactory<>("diagnosis"));
+			treatmentId.setCellValueFactory(
+					data -> new SimpleStringProperty(Integer.toString(data.getValue().getTreatmentId())));
+
+			try {
+
+				treatmentsTable.getColumns().addAll(treatmentId, startDate, duration, medication,
+						diagnosis, recommendation);
+
+				treatmentsTable.getItems().addAll(treatments);
+			} catch (Exception e) {
+				e.printStackTrace();
+				ErrorPopup.errorPopup(0);
+				return;
+			}
+		}
+	}
+
+	public void displayAllTreatmentsView(ActionEvent aEvent) throws IOException {
+		hideAll();
+		consultTreatmentsView.setDisable(false);
+		consultTreatmentsView.setVisible(true);
+		try {
+			setTreatmentTables();
+		} catch (Exception e) {
+			ErrorPopup.errorPopup(0);
+			e.printStackTrace();
+			return;
+		}
+	}
    
 	private Parent root;
 	private Stage stage;
@@ -142,6 +166,18 @@ public class PatientMenuController {
 		stage.setScene(scene);
 		stage.show();
 
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		treatmentId.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getTreatmentId())));
+		startDate.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getStartDate().toString()));
+		duration.setCellValueFactory(data -> new SimpleStringProperty(Integer.toString(data.getValue().getDuration())));
+    	medication.setCellValueFactory(new PropertyValueFactory<>("medication"));
+    	recommendation.setCellValueFactory(new PropertyValueFactory<>("adviceTreatment"));
+    	diagnosis.setCellValueFactory(new PropertyValueFactory<>("diagnosis"));
+		
 	}
 
 }
